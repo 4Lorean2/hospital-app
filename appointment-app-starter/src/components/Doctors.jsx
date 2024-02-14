9f08d3bb5e1509d5dfad9df3a4330b060a143280
Mustafa Taşdöğen <1Mustafatasdogen@gmail.com>
2024-02-14 17:19:44 +0300

    adding modal

--- appointment-app-starter/src/components/Doctors.jsx
+++ appointment-app-starter/src/components/Doctors.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap/";
 import { doctorData } from "../helper/data";
+import AddModal from "./AddModal";
 
 const Doctors = () => {
+  const [show, setShow] = useState(false);
+  const [drName, setDrName] = useState("");
+
+  const handleClose = () => setShow(false);
+  const handleShow = (name) => {
+    setShow(true);
+    setDrName(name);
+  };
+
   return (
     <Container className="p-2">
       <h3 className="display-6 mb-3" style={{ color: "rgb(166, 18, 189)" }}>
@@ -11,12 +22,18 @@ const Doctors = () => {
       <Row>
         {doctorData.map(({ id, img, dep, name }) => (
           <Col key={id}>
-            <img src={img} alt={name} className="img-thumbnail doctor-img " />
+            <img
+              src={img}
+              alt={name}
+              className="img-thumbnail doctor-img  "
+              onClick={() => handleShow(name)}
+            />
             <h5>{name}</h5>
             <h6>{dep}</h6>
           </Col>
         ))}
       </Row>
+      <AddModal show={show} handleClose={handleClose} drName={drName} />
     </Container>
   );
 };
